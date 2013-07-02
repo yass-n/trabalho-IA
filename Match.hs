3884b16cd6d96601d62ebd913cbe7c20ddde7fde
yassin <a11011BCC052@labs.facom.ufu.br>
2013-07-02 12:10:20 -0300

    Modulo match, usando notação do, main com testes

---
+++ Match.hs
@@ -0,0 +1,64 @@
+module Match
+( match
+, Termo(..)
+, Predicado(..)
+) where
+
+type Variavel = Char
+
+type Constante = String
+
+type Ligacao = (Variavel, Constante)
+
+-- Um termo pode ser uma variável ou uma constante
+data Termo = Variavel Variavel | Constante Constante deriving (Eq, Show)
+
+-- Um predicado é composto pelo nome (String) e uma lista de Termos
+data Predicado = Predicado String [Termo] deriving (Show)
+
+-- Adiciona uma ligação a lista de ligações. A variável '_' não casa com nenhuma
+-- constante, logo devem ser ignoradas as ligações com '_'
+adicionaLigacao :: Variavel -> Constante -> [Ligacao] -> [Ligacao]
+adicionaLigacao '_' _ ls = ls
+adicionaLigacao v c [] = [(v,c)]
+adicionaLigacao v c ls = (v,c):ls
+
+-- Encontra uma ligacao dada uma Variavel
+encontraLigacao :: Variavel -> [Ligacao] -> Maybe Ligacao
+encontraLigacao v [] = Nothing
+encontraLigacao '_' _ = Nothing
+encontraLigacao v1 ((v2, c):t)
+	| v1 == v2 = Just (v2, c)
+	| otherwise = encontraLigacao v1 t
+
+-- Match os termos de dois predicados se tiverem o mesmo nome, caso contrário Nothing
+match :: Predicado -> Predicado -> Maybe [Ligacao]
+match (Predicado n1 t1) (Predicado n2 t2)
+    | n1 == n2 = matchTermos t1 t2 []
+    | otherwise = Nothing
+
+-- Match os termos de dois predicados
+-- bug? se o primeiro elemento da lista de termos for uma variável o match não
+-- trata os outros termos
+matchTermos :: [Termo] -> [Termo] -> [Ligacao] -> Maybe [Ligacao]
+matchTermos [] [] ls = Just ls
+matchTermos ((Variavel v1):t1) (h2:t2) ls = do -- O 1º elemento de 1ª lista é uma variável
+    ls2 <- matchVariavel (Variavel v1) h2 ls
+    ls3 <- matchTermos t1 t2 ls2
+    return ls3
+matchTermos (h1:[]) (h2:[]) ls = matchAtomos h1 h2 ls                        -- Cada lista de temos tem apenas um atomo
+matchTermos (h1:t1) (h2:t2) ls = do
+    ls2 <- matchTermos [h1] [h2] ls
+    ls3 <- matchTermos t1 t2 ls2
+    return ls3
+
+matchAtomos :: Termo -> Termo -> [Ligacao] -> Maybe [Ligacao]
+matchAtomos a1 a2 ls
+    | a1 == a2 = Just ls
+    | otherwise  = Nothing
+
+matchVariavel :: Termo -> Termo -> [Ligacao] -> Maybe [Ligacao]
+matchVariavel (Variavel v1) (Constante c1) ls =
+    case ligacao of Just (v2, c2) -> matchTermos [(Constante c2)] [(Constante c1)] ls
+                    Nothing -> Just (adicionaLigacao v1 c1 ls)
+    where ligacao = encontraLigacao v1 ls
---
+++ teste.hs
@@ -0,0 +1,39 @@
+import Match
+
+main:: IO ()
+main = do
+    putStrLn "teste do match"
+
+    let x = Variavel 'X'
+    let y = Variavel 'Y'
+    let underscore = Variavel '_'
+    
+    let apple = Constante "apple"
+    let red = Constante "red"
+    let orange = Constante "orange"
+
+    let p1 = Predicado "color" [x,red]
+    let p2 = Predicado "color" [apple, red]
+    let p3 = Predicado "color" [apple, y]
+    let p4 = Predicado "color" [x,y]
+    let p5 = Predicado "color" [underscore, red]
+    let p6 = Predicado "color" [underscore, underscore]
+    let p7 = Predicado "color" [apple, orange]
+    let p8 = Predicado "color" [x,x]
+    
+    let t1 = match p1 p2 == Just [('X', "apple")]
+    let t2 = match p3 p2 == Just [('Y', "red")]
+    let t3 = match p4 p2 == Just [('Y', "red"), ('X', "apple")]
+    let t4 = match p2 p2 == Just []
+    let t5 = match p5 p2 == Just []
+    let t6 = match p6 p2 == Just []
+    let t7 = match p7 p2 == Nothing
+    let t8 = match p8 p2 == Nothing
+    
+    
+    mapM_ testa [t1, t2, t3, t4, t5, t6, t7, t8]
+
+testa :: Bool -> IO ()
+testa t = do
+    if t then putStrLn " OK"
+         else putStrLn " Fail"
