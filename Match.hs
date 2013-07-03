f057eddc78ca9f148e90a468f20b3f98abc8edc4
Yassin <yassin791@gmail.com>
2013-07-02 22:46:32 -0300

    removidas coisas do basics

--- Match.hs
+++ Match.hs
@@ -4,30 +4,7 @@ module Match
 , Predicado(..)
 ) where
 
-type Variavel = Char
-
-type Constante = String
-
-type Ligacao = (Variavel, Constante)
-
--- Um termo pode ser uma variável ou uma constante
-data Termo = Variavel Variavel | Constante Constante deriving (Eq, Show)
-
--- Um predicado é composto pelo nome (String) e uma lista de Termos
-data Predicado = Predicado String [Termo] deriving (Show)
-
--- Adiciona uma ligação a lista de ligações. A variável '_' não casa com nenhuma
--- constante, logo devem ser ignoradas as ligações com '_'
-adicionaLigacao :: Variavel -> Constante -> [Ligacao] -> [Ligacao]
-adicionaLigacao '_' _ ls = ls
-adicionaLigacao v c [] = [(v,c)]
-adicionaLigacao v c ls = (v,c):ls
-
--- Encontra uma ligacao dada uma Variavel
-encontraLigacao :: Variavel -> [Ligacao] -> Maybe Ligacao
-encontraLigacao v [] = Nothing
-encontraLigacao '_' _ = Nothing
-encontraLigacao v1 ((v2, c):t) = if v1 == v2 then Just (v2, c) else encontraLigacao v1 t
+import Basics
 
 -- Match os termos de dois predicados se tiverem o mesmo nome, caso contrário Nothing
 match :: Predicado -> Predicado -> Maybe [Ligacao]
