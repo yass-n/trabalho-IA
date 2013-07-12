2d9a6545839a8be2ac125589a878152e783e75b1
Yassin <yassin791@gmail.com>
2013-07-12 02:02:14 -0300

    match v2

--- Match.hs
+++ Match.hs
@@ -1,34 +1,16 @@
-module Match
-( match
-, Termo(..)
-, Predicado(..)
-) where
+module Match where
 
-import Basics
+import Tipos
 
-type Lig = Ligacao Variavel Constante
+match :: Expressao String -> Expressao String -> [Ligacao String] -> Maybe ([Ligacao String])
+match (Variavel "_") _ ligacoes = Just ligacoes
 
--- Match os termos de dois predicados se tiverem o mesmo nome, caso contrário Nothing
-match :: Predicado -> Predicado -> Maybe [Lig]
-match (Predicado n1 t1) (Predicado n2 t2)
-    | n1 == n2 = matchTermos t1 t2 []
-    | otherwise = Nothing
+match (Atomo p) (Atomo d) ligacoes = if p == d then Just ligacoes else Nothing
 
--- Match os termos de dois predicados
-matchTermos :: [Termo] -> [Termo] -> [Lig] -> Maybe [Lig]
-matchTermos [] [] ls = Just ls
-matchTermos ((Variavel v1):t1) (h2:t2) ls =                  -- O 1º elemento de 1ª lista é uma variável
-    matchVariavel (Variavel v1) h2 ls >>= matchTermos t1 t2
-matchTermos (h1:[]) (h2:[]) ls = matchAtomos h1 h2 ls        -- Cada lista de temos tem apenas um atomo
-matchTermos (h1:t1) (h2:t2) ls =
-    matchTermos [h1] [h2] ls >>= matchTermos t1 t2
+match (Variavel p) d ligacoes =
+    case axaLigacao (Variavel p) ligacoes of
+        Just (p,b) -> match b d ligacoes
+        Nothing -> Just (addLigacao (Variavel p) d ligacoes)
 
-matchAtomos :: Termo -> Termo -> [Lig] -> Maybe [Lig]
-matchAtomos a1 a2 ls = if a1 == a2 then Just ls else Nothing
-
-matchVariavel :: Termo -> Termo -> [Lig] -> Maybe [Lig]
-matchVariavel (Variavel '_') _ ls = Just ls                  -- Ignora '_'
-matchVariavel (Variavel v1) (Constante c1) ls =
-    case encontraLigacao v1 ls of
-        Just (Ligacao v2 c2) -> matchTermos [(Constante c2)] [(Constante c1)] ls
-        Nothing -> Just (adicionaLigacao v1 c1 ls)
+match (Seq firstP restP) (Seq firstD restD) ligacoes =
+    match firstP firstD ligacoes >>=  match restP restD
