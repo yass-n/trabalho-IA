6ec8380d27c915d88a8021727aca7d705e774e4d
viniciusfm <viniciusmacielf@gmail.com>
2013-07-15 00:38:17 -0300

    primeiras funcoes funcionando

--- Streams.hs
+++ Streams.hs
@@ -1,6 +1,14 @@
 module Stream where
 
-data ObjectStream a = EmptyStream | Stream a (ObjectStream a) deriving (Show, Read, Eq)
+data ObjectStream a = EmptyStream | Object a | Stream (ObjectStream a) (ObjectStream a) deriving (Show, Read, Eq)
+
+{-
+	EXEMPLOS DE USO:
+	* Criando um Stream:
+		let x = EmptyStream															"Stream Vazia"
+		let y = Stream (Object "object1") EmptyStream								"Stream com um objeto"
+		let z = Stream (Object "object1") (Stream (Object "object2") EmptyStream)	"Stream com dois objetos"
+-}
 
 stream_endp :: ObjectStream a -> Bool
 stream_endp EmptyStream = True
@@ -9,13 +17,13 @@ stream_endp (Stream _ _) = False
 stream_first :: ObjectStream a -> ObjectStream a
 stream_first (Stream first _) = first
 
---stream_rest :: ObjectStream a -> ObjectStream a
---stream_rest (Stream _ rest) = rest
+stream_rest :: ObjectStream a -> ObjectStream a
+stream_rest (Stream _ rest) = rest
 
---stream_cons :: ObjectStream a -> ObjectStream a -> ObjectStream a
---stream_cons objeto stm = (Stream objeto stm)
+stream_cons :: ObjectStream a -> ObjectStream a -> ObjectStream a
+stream_cons objeto stm = (Stream objeto stm)
 
---stream_append :: ObjectStream a -> ObjectStream a -> ObjectStream a
---stream_append stm1 stm2 =
---	if (stream_endp stm1) then stm2 
---	else stream_cons (stream_first stm1) (stream_append (stream_rest stm1) stm2)
+stream_append :: ObjectStream a -> ObjectStream a -> ObjectStream a
+stream_append stm1 stm2 =
+	if (stream_endp stm1) then stm2 
+	else stream_cons (stream_first stm1) (stream_append (stream_rest stm1) stm2)
