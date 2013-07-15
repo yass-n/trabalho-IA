7bf385a84a630e9ca9b4c0db339e93c36de8d7c0
viniciusfm <viniciusmacielf@gmail.com>
2013-07-15 00:56:04 -0300

    concatenate function implementada funcionando

--- Streams.hs
+++ Streams.hs
@@ -27,3 +27,11 @@ stream_append :: ObjectStream a -> ObjectStream a -> ObjectStream a
 stream_append stm1 stm2 =
 	if (stream_endp stm1) then stm2 
 	else stream_cons (stream_first stm1) (stream_append (stream_rest stm1) stm2)
+
+stream_concatenate :: ObjectStream a -> ObjectStream a
+stream_concatenate streams
+	| stream_endp streams = EmptyStream
+	| stream_endp (stream_first streams) = stream_concatenate (stream_rest streams)
+	| otherwise = stream_cons (stream_first (stream_first streams)) 
+								(stream_concatenate (stream_cons (stream_rest (stream_first streams))
+													(stream_rest streams)))
