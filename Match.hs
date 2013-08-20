2a79329179c681c274be78a572060d2ef0501bea
Yassin <yassin791@gmail.com>
2013-08-20 15:50:05 -0300

    No match uma variável não pode casar com uma sequencia

--- Match.hs
+++ Match.hs
@@ -7,6 +7,8 @@ match Ign _ ligacoes = Just ligacoes
 
 match (Atomo p) (Atomo d) ligacoes = if p == d then Just ligacoes else Nothing
 
+match (Variavel _) (Seq _ _) _ = Nothing
+
 match (Variavel p) d ligacoes =
     case axaLigacao (Variavel p) ligacoes of
         Just (p,b) -> match b d ligacoes
